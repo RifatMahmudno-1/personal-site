3a50eb10d0711f724072f55e783af34e9799546f
Rifat Mahmud <rifatmahmudpc@gmail.com>
2024-08-27 18:19:04 +0600

    pagination added

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,33 @@
 <template>
 	<div class="flex gap-2 flex-col">
 		<div class="text-center bg-cyan-200 p-2 rounded text-lg" v-if="pending">Loading posts...</div>
-		<PostView v-for="d in data" :postData="d" @update-data="updateData" v-else-if="data?.length" @delete-data="deleteData" :key="d._id" />
-		<div class="text-center bg-cyan-200 p-2 rounded text-lg" v-else>No posts found</div>
+		<slot v-else>
+			<PostView v-for="d in data.result" :postData="d" @update-data="updateData" v-if="data?.result.length" @delete-data="deleteData" :key="d._id" />
+			<div class="text-center bg-cyan-200 p-2 rounded text-lg" v-else>No posts found</div>
+		</slot>
+		<div class="flex justify-between gap-2" v-if="!pending">
+			<button :disabled="page <= 1" @click="() => router.push({ query: { page: page - 1 === 1 ? undefined : page - 1 } })" class="bg-cyan-300 px-2 py-1 rounded shadow hover:shadow-lg transition-shadow">Previous Page</button>
+			<button :disabled="!data?.hasMore" @click="() => router.push({ query: { page: page + 1 } })" class="bg-cyan-300 px-2 py-1 rounded shadow hover:shadow-lg transition-shadow">Next Page</button>
+		</div>
 	</div>
-	<PostCreate @update-data="updateData" v-if="$route.meta.admin" />
+	<PostCreate @update-data="updateData" v-if="route.meta.admin" />
 </template>
 
 <script setup lang="ts">
+	const route = useRoute()
+	const router = useRouter()
 	const refreshSidebar = inject<Ref<boolean>>('refreshSidebar')
+	const page = computed<number>(() => {
+		const p = Number(route.query.page)
+		if (p <= 0 || !Number.isInteger(p)) return 1
+		return p
+	})
 
 	definePageMeta({
 		layout: 'advanced'
 	})
 
-	const { data, pending, error } = await cLazyFetch<EachPostType[]>('/api/posts', { responseType: 'json' })
+	const { data, pending, error } = await cLazyFetch<{ hasMore: boolean; result: EachPostType[] }>('/api/posts', { query: { page }, responseType: 'json', watch: [page] })
 
 	watchEffect(() => {
 		if (error.value) showError(error.value)
@@ -22,16 +35,25 @@
 
 	function updateData(d: EachPostType) {
 		if (pending.value || !data.value) return
-		const ind = data.value.findIndex(el => el._id === d._id)
-		if (ind === -1) data.value.unshift(d)
-		else data.value[ind] = d
+		const ind = data.value.result.findIndex(el => el._id === d._id)
+		if (ind === -1) data.value.result.unshift(d)
+		else data.value.result[ind] = d
 		refreshSidebar!.value = true
 	}
 
 	function deleteData(_id: EachPostType['_id']) {
 		if (pending.value || !data.value) return
-		const ind = data.value.findIndex(el => el._id === _id)
-		if (ind !== -1) data.value.splice(ind, 1)
+		const ind = data.value.result.findIndex(el => el._id === _id)
+		if (ind !== -1) data.value.result.splice(ind, 1)
 		refreshSidebar!.value = true
 	}
+
+	onMounted(() => {
+		const p = Number(route.query.page)
+		if (p <= 1 || !Number.isInteger(p)) {
+			const q = { ...route.query }
+			delete q.page
+			router.replace({ path: route.path, query: q, hash: route.hash })
+		}
+	})
 </script>
--- server/api/posts/index.get.ts
+++ server/api/posts/index.get.ts
@@ -1,12 +1,21 @@
 type QueryType = {
 	section?: string
+	page: string
 }
 
 const schema: AJVSchema<QueryType> = {
 	type: 'object',
 	properties: {
-		section: { type: 'string', nullable: true }
+		section: { type: 'string', nullable: true },
+		page: {
+			type: 'string',
+			ValidateByFunction: (data: string) => {
+				const n = Number(data)
+				return n >= 1 && Number.isInteger(n)
+			}
+		}
 	},
+	required: ['page'],
 	additionalProperties: false
 }
 
@@ -17,15 +26,18 @@ export default defineEventHandler(async ev => {
 		const validation = ajvValidate(schema, query)
 		if (validation.error) return res.setStatus(400).send(validation)
 
+		const perPage = Number(process.env.PerPage)
 		const got = await mongo
 			.client!.db('Personal_Site')
 			.collection('Posts')
 			.find(query.section ? { section: query.section } : {})
 			.sort({ createdAt: -1 })
+			.skip((Number(query.page) - 1) * perPage)
+			.limit(perPage + 1)
 			.toArray()
 
 		if (query.section && !got.length) return res.setStatus(404)
-		return res.send(got)
+		return res.send({ hasMore: got.length > perPage, result: got.slice(0, perPage) })
 	} catch (e) {
 		console.log(e)
 		return res.sendStatus(500)
